042ded78190e0e7f226f7b6de8dd163e936b7dce
hws325 <hws325@qq.com>
2025-12-30 22:07:26 +0800

    Update config.js
    修改了英雄区（首页顶部打卡）的标题。

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,10 +18,10 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
+  HEO_HERO_TITLE_1: '窥探AI',
+  HEO_HERO_TITLE_2: '与拓展技能',
+  HEO_HERO_TITLE_3: '插上AI翅膀',
+  HEO_HERO_TITLE_4: '探索世界',
   HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
   HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
   // 英雄区遮罩文字
