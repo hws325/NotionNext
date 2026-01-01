ebc6e3b4f42b5b30ba21977417a94632e8a4e1fb
hws325 <hws325@qq.com>
2026-01-01 18:08:26 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,16 +20,16 @@ const CONFIG = {
   HEO_HERO_TITLE_1: '窥探AI',
   HEO_HERO_TITLE_2: '与拓展技能',
   HEO_HERO_TITLE_3: '插上AI翅膀',
-  HEO_HERO_TITLE_4: '探索世界',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_4: 'Hi,welcome,跟我一起',
+  HEO_HERO_TITLE_5: '轻松学AI  掌握技能 提高效率',
+  HEO_HERO_TITLE_LINK: 'https://blog.erai.fun/',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_2: { title: '实用教程', url: '/tag/实用教程'},
+  HEO_HERO_CATEGORY_3: { title: '热门文章', url: '/tag/热门文章' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -50,9 +50,9 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://blog.erai.fun/',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL3: 'https://blog.erai.fun/',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -115,7 +115,7 @@ const CONFIG = {
     }
   ],
 
-  HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
+  HEO_SOCIAL_CARD:false, // 是否显示右侧，点击加入社群按钮
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
